d947c859ccc87390fbb4cf042d30dd5ebeca00b4
shuoye <shuoye537300@gmail.com>
2026-07-08 01:17:01 +0800

    feat(import): 添加 Nikki4 版本的 AnimationClip 和 Material 支持
    - 新增 AnimationClip_Nikki4 类，扩展 AnimationClip_2018_3，添加定制 ACL 字段
    - 实现 AnimationClip_Nikki4 的 ReadRelease 方法，支持自定义数据读取
    - 新增 Material_Nikki4 类，继承 Material_2018_3，实现自定义材质读取逻辑
    - 在 GameAssetFactory 中添加对 AnimationClip_Nikki4 和 Material_Nikki4 的创建支持
    - 在读取失败时保存数据至本地，便于调试和错误分析
    - 修正了 Texture 读取时的代码格式和注释提示

---
+++ Source/AssetRipper.Import/AssetCreation/Nikki4/Material_Nikki4.cs
@@ -0,0 +1,25 @@
+﻿using AssetRipper.Assets.Metadata;
+using AssetRipper.IO.Endian;
+using AssetRipper.SourceGenerated.Classes.ClassID_21;
+
+namespace AssetRipper.Import.AssetCreation.Nikki4;
+
+public class Material_Nikki4(AssetInfo info) : Material_2018_3(info)
+{
+	protected uint m_MaterialType;
+
+	public override void ReadRelease(ref EndianSpanReader reader)
+	{
+		this.Name = reader.ReadRelease_Utf8StringAlign();
+		this.Shader_C21.ReadRelease(ref reader);
+		this.ShaderKeywords_C21_Utf8String = reader.ReadRelease_Utf8StringAlign();
+		this.LightmapFlags_C21 = reader.ReadUInt32();
+		this.EnableInstancingVariants_C21 = reader.ReadBoolean();
+		this.DoubleSidedGI_C21 = reader.ReadRelease_BooleanAlign();
+		this.CustomRenderQueue_C21 = reader.ReadInt32();
+		this.m_MaterialType = reader.ReadUInt32();
+		this.StringTagMap_C21.ReadRelease_Map_Utf8StringAlign_Utf8StringAlign(ref reader);
+		this.DisabledShaderPasses_C21.ReadRelease_ArrayAlign_Utf8StringAlign(ref reader);
+		this.SavedProperties_C21.ReadRelease(ref reader);
+	}
+}
